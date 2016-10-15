36e75bff8e2ce6ea45957d75419b119bf579a750
Tamas Kocsis <kocsis.tamas88@gmail.com>
2016-10-15 12:50:48 +0200

    initial setup
    Portfolio Site starting point

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'sm',
+            width: 320,
+            suffix: '320',
+            quality: 60
+          }, {
+            name: 'med',
+            width: 640,
+            suffix: '640',
+            quality: 60
+          }, {
+            name: 'lg',
+            width: 800,
+            separator: '-',
+            suffix: '800',
+            quality: 60
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
